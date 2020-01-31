5c6b066595adf45600a9d3dcdbe809598297aa9a
Mustafa <mustafazaidi840@gmail.com>
2020-01-31 17:57:54 +0500

    initial commit

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
+from django.http import HttpResponse
+
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("we are at contact")
+    return HttpResponse("We are at contact")
 
 def tracker(request):
-    return HttpResponse("we are at tracker")
+    return HttpResponse("We are at tracker")
 
 def search(request):
-    return HttpResponse("we are at search")
+    return HttpResponse("We are at search")
 
-def productview(request):
-    return HttpResponse("we are at productview")
+def productView(request):
+    return HttpResponse("We are at productview")
 
 def checkout(request):
-    return HttpResponse("we are at checkout")
+    return HttpResponse("We are at checkout")
 
 
 
